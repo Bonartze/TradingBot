8c5d91099ce822a2fce3c34546493ce8869e49f9
Ivan Kirilin <iokirilin@mail.ru>
2025-02-21 14:32:05 +0300

    improve/pipeline-extending

--- Backtesting/src/load_data_some_years.py
+++ Backtesting/src/load_data_some_years.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for year in range(2020, 2026):
+for year in range(2020, 2025):
     result = subprocess.run(
         ["python3", "binance_stats.py", "BTCUSDT", "5m", str(year)],
         capture_output=True,
