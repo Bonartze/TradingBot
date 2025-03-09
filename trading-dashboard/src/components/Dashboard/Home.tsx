f633099212165f07541252814362fe5e26b2de29
Ivan Kirilin <iokirilin@mail.ru>
2025-03-09 19:07:05 +0300

    minor fix

--- trading-dashboard/src/components/Dashboard/Home.tsx
+++ trading-dashboard/src/components/Dashboard/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Container, Typography, Box, Paper } from '@mui/material';
+import {Container, Typography, Paper} from '@mui/material';
 
 const Home: React.FC = () => {
     return (
-        <Container maxWidth="md" sx={{ mt: 4 }}>
-            <Paper elevation={3} sx={{ p: 3 }}>
+        <Container maxWidth="md" sx={{mt: 4}}>
+            <Paper elevation={3} sx={{p: 3}}>
                 <Typography variant="h4" gutterBottom>
                     Добро пожаловать в Trading Dashboard
                 </Typography>
