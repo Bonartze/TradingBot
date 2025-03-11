a6e29935e3829db180d62f0a86e1fabec88c2632
Ivan Kirilin <101982599+Bonartze@users.noreply.github.com>
2025-03-11 15:08:46 +0300

    Fix/backtesting arima granch, added ARIMA-GARCH backtesting (#26)
    * check
    
    * hotfix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fixes
    
    * fix/backtesting-arima-garch, added arima-garch backtesting
    
    * fix
    
    * up margin
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * added data
    
    * minor fix
    
    * fix
    
    ---------
    
    Co-authored-by: Ivan Kirilin <iokirilin@mail.ru>

--- Backtesting/src/arima_garch_adaptive_backtesting.cpp
+++ Backtesting/src/arima_garch_adaptive_backtesting.cpp
@@ -1,9 +1,19 @@
 #include "../include/arima_garch_adaptive_backtesting.h"
+#include <filesystem>
+#include <vector>
+#include <chrono>
+#include <tuple>
+#include <map>
+#include <regex>
+#include <array>
+#include <sstream>
+
 constexpr size_t WINDOW_SIZE = 100;
-constexpr size_t EXPECTED_PROFIT_MARGIN = 200;
+constexpr size_t EXPECTED_PROFIT_MARGIN = 50;
+constexpr std::array<double, 12> EXPECTED_PROFIT = {-12, -35, 0, -54, -53, -34, 122, 48, -6, 85, 277, 552};
 
 void ArimaGarchAdaptiveBacktesting::SetUp() {
-    test_file = "../data/Data_extracted/5m/BTCUSDT/2021/BTCUSDT-5m-2021-02.csv";
+    test_file = "../data/Data_extracted/5m/BTCUSDT/2020/BTCUSDT-5m-2020-01.csv";
     arima_garch_adaptive_backtesting = std::make_unique<ArimaGarchAdaptive>(test_file);
 }
 
@@ -11,15 +21,26 @@ void ArimaGarchAdaptiveBacktesting::TearDown() {
     arima_garch_adaptive_backtesting.reset();
 }
 
-constexpr std::array<double, 5> EXPECTED_PROFIT = {1200, -150, 200, 400, -700};
+namespace {
+    int extractMonth(const std::string &filename) {
+        std::regex re(".*-(\\d{4})-(\\d{2})\\.csv");
+        std::smatch match;
+        if (std::regex_match(filename, match, re)) {
+            return std::stoi(match[2]);
+        }
+        return 0;
+    }
 
-TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarchMultiYearBacktesting) {
-    std::filesystem::create_directories("../data/arima_garch/");
+    TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarch2020Backtesting) {
+        std::filesystem::create_directories("../data/arima_garch/");
 
-    for (size_t year = 2020; year <= 2024; ++year) {
+        const size_t year = 2020;
         const std::string data_dir = "../data/Data_extracted/5m/BTCUSDT/" + std::to_string(year);
         double total_profit = 0.0;
         size_t total_trades = 0;
+
+        std::map<int, double> monthly_profit;
+        std::map<int, size_t> monthly_trades;
 
         std::vector<std::filesystem::path> files;
         for (const auto &entry: std::filesystem::directory_iterator(data_dir)) {
@@ -36,18 +57,21 @@ TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarchMultiYearBacktesting) {
         Logger(LogLevel::INFO) << "Year: " << year << " | Total files: " << files.size();
 
         for (const auto &file: files) {
-            Logger(LogLevel::INFO) << "Processing file: " << file.string();
+            int month = extractMonth(file.filename().string());
+            if (month == 0) {
+                Logger(LogLevel::WARNING) << "Warning: Could not extract month from file " << file.string();
+                continue;
+            }
+            Logger(LogLevel::INFO) << "Processing file: " << file.string() << " | Month: " << month;
             auto start_time = std::chrono::high_resolution_clock::now();
 
             auto candles = loadCandles(file.string());
             auto prices = TradingStrategy::extract_prices(candles);
-
             if (prices.empty()) {
                 Logger(LogLevel::WARNING) << "Warning: No prices extracted from file " << file;
                 continue;
             }
 
-            // Лог для конкретного файла
             const std::string output_file = "../data/arima_garch/" + std::to_string(year) + "_" + file.filename().
                                             string();
             CSVLogger csv_logger(output_file);
@@ -55,6 +79,9 @@ TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarchMultiYearBacktesting) {
             auto [profit, trades] = arima_garch_adaptive_backtesting->wrapper_execute(WINDOW_SIZE, prices, csv_logger);
             total_profit += profit;
             total_trades += trades;
+
+            monthly_profit[month] += profit;
+            monthly_trades[month] += trades;
 
             auto end_time = std::chrono::high_resolution_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
@@ -67,6 +94,15 @@ TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarchMultiYearBacktesting) {
                 << " | Total Profit: " << total_profit
                 << " | Total Trades: " << total_trades;
 
-        EXPECT_NEAR(EXPECTED_PROFIT[year - 2020], total_profit, EXPECTED_PROFIT_MARGIN);
+        std::stringstream ss;
+        ss << "Monthly Profit:\n";
+        for (const auto &pair: monthly_profit) {
+            int month = pair.first;
+            double profit = pair.second;
+            size_t trades = monthly_trades[month];
+            ss << "Month " << month << ": Profit = " << profit << ", Trades = " << trades << "\n";
+            EXPECT_NEAR(EXPECTED_PROFIT[month-1], profit, EXPECTED_PROFIT_MARGIN);
+        }
+        Logger(LogLevel::INFO) << ss.str();
     }
 }
--- Backtesting/src/scalping_backtesting.cpp
+++ Backtesting/src/scalping_backtesting.cpp
@@ -4,7 +4,6 @@
 #include "../../TradingStrategies/Common/include/TradingStrategy.h"
 #include <filesystem>
 
-
 constexpr double START_BALANCE = 1000.0;
 constexpr size_t START_YEAR = 2020;
 constexpr size_t END_YEAR = 2024;
@@ -13,47 +12,38 @@ constexpr size_t SCALPING_SMA_SHORT = 5.0;
 constexpr size_t SCALPING_SMA_LONG = 10.0;
 constexpr size_t SCALPING_RSI_OVERSOLD = 30.0;
 constexpr size_t SCALPING_RSI_OVERBOUGHT = 29.0;
-constexpr size_t EXPECTED_PROFIT_MARGIN = 10;
-
+constexpr size_t EXPECTED_PROFIT_MARGIN = 30;
 constexpr std::array<double, 5> EXPECTED_PROFIT = {40, 2, -20, 60, -80};
 
 void ScalpingBacktesting::SetUp() {
-    //data_dir = "/home/ivan/CLionProjects/TradingBot/Backtesting/data/Data_extracted/5m/BTCUSDT/";
+    // data_dir можно установить здесь, если нужно
 }
 
 void ScalpingBacktesting::TearDown() {
 }
 
 namespace {
-    void processYearData(const std::string &data_dir, ScalpingStr &scalp, double &total_profit, size_t &total_trades,
-                         CSVLogger &csv_logger) {
+    void processYearData(const std::string &data_dir, ScalpingStr &scalp, double &total_profit, size_t &total_trades, CSVLogger &csv_logger) {
         std::vector<std::filesystem::path> files;
-
         for (const auto &entry: std::filesystem::directory_iterator(data_dir)) {
             if (entry.path().extension() == ".csv") {
                 files.push_back(entry.path());
             }
         }
-
         std::sort(files.begin(), files.end(),
-                  [](const std::filesystem::path &file_a, const std::filesystem::path &file_b) {
-                      return file_a.filename().string() < file_b.filename().string();
+                  [](const std::filesystem::path &a, const std::filesystem::path &b) {
+                      return a.filename().string() < b.filename().string();
                   });
-
         Logger(LogLevel::INFO) << "Starting backtesting for directory: " << data_dir
                 << ". Total files: " << files.size();
-
         for (const auto &file: files) {
             Logger(LogLevel::INFO) << "Processing file: " << file;
-
             auto start_time = std::chrono::high_resolution_clock::now();
             auto candles = loadCandles(file);
             auto prices = TradingStrategy::extract_prices(candles);
             std::tie(total_profit, total_trades) = scalp.wrapper_execute(WINDOW_SIZE, prices, csv_logger);
-
             auto end_time = std::chrono::high_resolution_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
             Logger(LogLevel::INFO) << "File Processed: " << file
                     << " | Total Trades: " << total_trades
                     << " | Duration: " << duration << " ms";
@@ -71,17 +61,13 @@ TEST_F(ScalpingBacktesting, ScalpingBacktesting) {
     for (size_t year = START_YEAR; year <= END_YEAR; ++year) {
         std::string output_file = "../data/scalping/" + std::to_string(year) + ".csv";
         CSVLogger csv_logger(output_file);
-
         double total_profit = 0.0;
         size_t total_trades = 0;
-
         processYearData("../data/Data_extracted/5m/BTCUSDT/"+std::to_string(year), scalp, total_profit, total_trades, csv_logger);
-
         Logger(LogLevel::INFO) << "Year: " << year
                 << " | Expected Profit: " << EXPECTED_PROFIT[year - START_YEAR]
                 << " | Calculated Profit: " << total_profit
                 << " | Total Trades: " << total_trades;
-
         EXPECT_NEAR(EXPECTED_PROFIT[year - START_YEAR], total_profit, EXPECTED_PROFIT_MARGIN);
     }
 }
