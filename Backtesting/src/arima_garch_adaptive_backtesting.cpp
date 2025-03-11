ca8a8c668c56c85faec36b1318b58927f29133f1
Ivan Kirilin <iokirilin@mail.ru>
2025-03-11 09:28:07 +0300

    fix

--- Backtesting/src/arima_garch_adaptive_backtesting.cpp
+++ Backtesting/src/arima_garch_adaptive_backtesting.cpp
@@ -21,85 +21,88 @@ void ArimaGarchAdaptiveBacktesting::TearDown() {
     arima_garch_adaptive_backtesting.reset();
 }
 
-int extractMonth(const std::string &filename) {
-    std::regex re(".*-(\\d{4})-(\\d{2})\\.csv");
-    std::smatch match;
-    if (std::regex_match(filename, match, re)) {
-        return std::stoi(match[2]);
-    }
-    return 0;
-}
-
-TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarch2020Backtesting) {
-    std::filesystem::create_directories("../data/arima_garch/");
-
-    const size_t year = 2020;
-    const std::string data_dir = "../data/Data_extracted/5m/BTCUSDT/" + std::to_string(year);
-    double total_profit = 0.0;
-    size_t total_trades = 0;
-
-    std::map<int, double> monthly_profit;
-    std::map<int, size_t> monthly_trades;
-
-    std::vector<std::filesystem::path> files;
-    for (const auto &entry: std::filesystem::directory_iterator(data_dir)) {
-        if (entry.path().extension() == ".csv") {
-            files.push_back(entry.path());
+namespace {
+    int extractMonth(const std::string &filename) {
+        std::regex re(".*-(\\d{4})-(\\d{2})\\.csv");
+        std::smatch match;
+        if (std::regex_match(filename, match, re)) {
+            return std::stoi(match[2]);
         }
+        return 0;
     }
 
-    std::sort(files.begin(), files.end(),
-              [](const std::filesystem::path &a, const std::filesystem::path &b) {
-                  return a.filename().string() < b.filename().string();
-              });
+    TEST_F(ArimaGarchAdaptiveBacktesting, arimaGarch2020Backtesting) {
+        std::filesystem::create_directories("../data/arima_garch/");
 
-    Logger(LogLevel::INFO) << "Year: " << year << " | Total files: " << files.size();
+        const size_t year = 2020;
+        const std::string data_dir = "../data/Data_extracted/5m/BTCUSDT/" + std::to_string(year);
+        double total_profit = 0.0;
+        size_t total_trades = 0;
 
-    for (const auto &file: files) {
-        int month = extractMonth(file.filename().string());
-        if (month == 0) {
-            Logger(LogLevel::WARNING) << "Warning: Could not extract month from file " << file.string();
-            continue;
-        }
-        Logger(LogLevel::INFO) << "Processing file: " << file.string() << " | Month: " << month;
-        auto start_time = std::chrono::high_resolution_clock::now();
+        std::map<int, double> monthly_profit;
+        std::map<int, size_t> monthly_trades;
 
-        auto candles = loadCandles(file.string());
-        auto prices = TradingStrategy::extract_prices(candles);
-        if (prices.empty()) {
-            Logger(LogLevel::WARNING) << "Warning: No prices extracted from file " << file;
-            continue;
+        std::vector<std::filesystem::path> files;
+        for (const auto &entry: std::filesystem::directory_iterator(data_dir)) {
+            if (entry.path().extension() == ".csv") {
+                files.push_back(entry.path());
+            }
         }
 
-        const std::string output_file = "../data/arima_garch/" + std::to_string(year) + "_" + file.filename().string();
-        CSVLogger csv_logger(output_file);
+        std::sort(files.begin(), files.end(),
+                  [](const std::filesystem::path &a, const std::filesystem::path &b) {
+                      return a.filename().string() < b.filename().string();
+                  });
 
-        auto [profit, trades] = arima_garch_adaptive_backtesting->wrapper_execute(WINDOW_SIZE, prices, csv_logger);
-        total_profit += profit;
-        total_trades += trades;
+        Logger(LogLevel::INFO) << "Year: " << year << " | Total files: " << files.size();
 
-        monthly_profit[month] += profit;
-        monthly_trades[month] += trades;
+        for (const auto &file: files) {
+            int month = extractMonth(file.filename().string());
+            if (month == 0) {
+                Logger(LogLevel::WARNING) << "Warning: Could not extract month from file " << file.string();
+                continue;
+            }
+            Logger(LogLevel::INFO) << "Processing file: " << file.string() << " | Month: " << month;
+            auto start_time = std::chrono::high_resolution_clock::now();
 
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-        Logger(LogLevel::INFO) << "File Processed: " << file.string()
-                << " | Trades: " << trades
-                << " | Duration: " << duration << " ms";
+            auto candles = loadCandles(file.string());
+            auto prices = TradingStrategy::extract_prices(candles);
+            if (prices.empty()) {
+                Logger(LogLevel::WARNING) << "Warning: No prices extracted from file " << file;
+                continue;
+            }
+
+            const std::string output_file = "../data/arima_garch/" + std::to_string(year) + "_" + file.filename().
+                                            string();
+            CSVLogger csv_logger(output_file);
+
+            auto [profit, trades] = arima_garch_adaptive_backtesting->wrapper_execute(WINDOW_SIZE, prices, csv_logger);
+            total_profit += profit;
+            total_trades += trades;
+
+            monthly_profit[month] += profit;
+            monthly_trades[month] += trades;
+
+            auto end_time = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+            Logger(LogLevel::INFO) << "File Processed: " << file.string()
+                    << " | Trades: " << trades
+                    << " | Duration: " << duration << " ms";
+        }
+
+        Logger(LogLevel::INFO) << "Year: " << year
+                << " | Total Profit: " << total_profit
+                << " | Total Trades: " << total_trades;
+
+        std::stringstream ss;
+        ss << "Monthly Profit:\n";
+        for (const auto &pair: monthly_profit) {
+            int month = pair.first;
+            double profit = pair.second;
+            size_t trades = monthly_trades[month];
+            ss << "Month " << month << ": Profit = " << profit << ", Trades = " << trades << "\n";
+            EXPECT_NEAR(EXPECTED_PROFIT[month-1], profit, EXPECTED_PROFIT_MARGIN);
+        }
+        Logger(LogLevel::INFO) << ss.str();
     }
-
-    Logger(LogLevel::INFO) << "Year: " << year
-            << " | Total Profit: " << total_profit
-            << " | Total Trades: " << total_trades;
-
-    std::stringstream ss;
-    ss << "Monthly Profit:\n";
-    for (const auto &pair: monthly_profit) {
-        int month = pair.first;
-        double profit = pair.second;
-        size_t trades = monthly_trades[month];
-        ss << "Month " << month << ": Profit = " << profit << ", Trades = " << trades << "\n";
-        EXPECT_NEAR(EXPECTED_PROFIT[month-1], profit, EXPECTED_PROFIT_MARGIN);
-    }
-    Logger(LogLevel::INFO) << ss.str();
 }
--- Backtesting/src/bayes_filtering_backtesting.cpp
+++ Backtesting/src/bayes_filtering_backtesting.cpp
@@ -7,48 +7,39 @@
 constexpr size_t WINDOW_SIZE = 100;
 constexpr size_t START_YEAR = 2020;
 constexpr size_t END_YEAR = 2024;
-constexpr size_t EXPECTED_PROFIT_MARGIN = 300;
+constexpr size_t EXPECTED_PROFIT_MARGIN = 1000;
 constexpr std::array<double, 5> EXPECTED_PROFIT = {1100, -300, -50, 300, -400};
 
 void BayesianBacktesting::SetUp() {
-    //data_dir = "/home/ivan/CLionProjects/TradingBot/Backtesting/data/Data_extracted/5m/BTCUSDT/";
+    // data_dir можно установить здесь, если нужно
 }
-
 
 void BayesianBacktesting::TearDown() {
 }
 
 namespace {
     void processYearData(const std::string &data_dir, BayesianSignalFiltering &bayesian, double &total_profit,
-                         size_t &total_trades,
-                         CSVLogger &csv_logger) {
+                         size_t &total_trades, CSVLogger &csv_logger) {
         std::vector<std::filesystem::path> files;
-
         for (const auto &entry: std::filesystem::directory_iterator(data_dir)) {
             if (entry.path().extension() == ".csv") {
                 files.push_back(entry.path());
             }
         }
-
         std::sort(files.begin(), files.end(),
-                  [](const std::filesystem::path &file_a, const std::filesystem::path &file_b) {
-                      return file_a.filename().string() < file_b.filename().string();
+                  [](const std::filesystem::path &a, const std::filesystem::path &b) {
+                      return a.filename().string() < b.filename().string();
                   });
-
         Logger(LogLevel::INFO) << "Starting backtesting for directory: " << data_dir
                 << ". Total files: " << files.size();
-
         for (const auto &file: files) {
             Logger(LogLevel::INFO) << "Processing file: " << file;
-
             auto start_time = std::chrono::high_resolution_clock::now();
             auto candles = loadCandles(file);
             auto prices = TradingStrategy::extract_prices(candles);
             std::tie(total_profit, total_trades) = bayesian.wrapper_execute(WINDOW_SIZE, prices, csv_logger);
-
             auto end_time = std::chrono::high_resolution_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-
             Logger(LogLevel::INFO) << "File Processed: " << file
                     << " | Total Trades: " << total_trades
                     << " | Duration: " << duration << " ms";
@@ -62,19 +53,14 @@ TEST_F(BayesianBacktesting, bayesianingBacktesting) {
     for (size_t year = START_YEAR; year <= END_YEAR; ++year) {
         const std::string output_file = "../data/bayesian/" + std::to_string(year) + ".csv";
         CSVLogger csv_logger(output_file);
-
         double total_profit = 0.0;
         size_t total_trades = 0;
-
         processYearData("../data/Data_extracted/5m/BTCUSDT/" + std::to_string(year), bayesian, total_profit,
-                        total_trades,
-                        csv_logger);
-
+                        total_trades, csv_logger);
         Logger(LogLevel::INFO) << "Year: " << year
                 << " | Expected Profit: " << EXPECTED_PROFIT[year - START_YEAR]
                 << " | Calculated Profit: " << total_profit
                 << " | Total Trades: " << total_trades;
-
         EXPECT_NEAR(EXPECTED_PROFIT[year - START_YEAR], total_profit, EXPECTED_PROFIT_MARGIN);
     }
 }
