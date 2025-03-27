6e09aacb8f4b8bc685b0417302ca036bdc7b6037
Ivan Kirilin <iokirilin@mail.ru>
2025-03-27 17:42:18 +0300

    added building in pipeline

---
+++ Scripts/build_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p ../TradingStrategies/build && cd ../TradingStrategies/build
+
+cmake ../ && \
+make -j`nproc` || echo "Error while generating strategy API" && \
+sudo make install
+
+echo "Strategy API is generated"
+
+
+mkdir -p ../../TradingEngine/build && \
+cd ../../TradingEngine/build
+cmake ../ && \
+make -j`nproc` || echo "Error while building server"
