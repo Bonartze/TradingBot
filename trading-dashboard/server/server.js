a2c91ce8a5192fb427b7de74cc9c26917ec4eff0
Ivan Kirilin <101982599+Bonartze@users.noreply.github.com>
2025-03-27 17:03:04 +0300

    Fix/https web severs (#54)
    * check
    
    * added
    
    * added
    
    * added
    
    * added
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Ivan Kirilin <iokirilin@mail.ru>

--- trading-dashboard/server/server.js
+++ trading-dashboard/server/server.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 
 const authRoutes = require('./routes/auth');
 const settingsRoutes = require('./routes/settings');
 const statisticsRoutes = require('./routes/statistics');
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-const HTTP_PORT = process.env.PORT || 5001;
-const PORT = 443;
-
-const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/backckkck.3utilities.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/backckkck.3utilities.com/fullchain.pem')
-};
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
@@ -29,13 +21,9 @@ mongoose
     .connect('mongodb://127.0.0.1:27017/tradingbot')
     .then(() => {
         console.log('Connected to MongoDB');
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`HTTP server running on port ${PORT}`);
 
-        app.listen(HTTP_PORT, '0.0.0.0', () => {
-            console.log(`HTTP server running on port ${HTTP_PORT}`);
-
-            https.createServer(sslOptions, app).listen(PORT, () => {
-                console.log(`HTTPS server running on port ${PORT}`);
-            });
-        })
-            .catch(err => console.error('MongoDB connection error:', err));
-    });
+        });
+    })
+    .catch(err => console.error('MongoDB connection error:', err));
