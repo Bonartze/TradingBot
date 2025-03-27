c4326fbf58a319c6fc78e44bc6702e370121ff23
Ivan Kirilin <101982599+Bonartze@users.noreply.github.com>
2025-03-27 17:16:34 +0300

    Fix/https web severs (#55)
    * check
    
    * added
    
    * added
    
    * added
    
    * added
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Ivan Kirilin <iokirilin@mail.ru>

--- trading-dashboard/server/routes/statistics.js
+++ trading-dashboard/server/routes/statistics.js
@@ -1,9 +1,9 @@
-// routes/statistics.js
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 const router = express.Router();
+
 router.get('/list', (req, res) => {
     const email = req.query.email;
     console.log('[DEBUG] /list called with email=', email);
@@ -14,37 +14,37 @@ router.get('/list', (req, res) => {
     fs.readdir(logsDir, (err, files) => {
         if (err) {
             console.error('[DEBUG] readdir error:', err);
-            return res.status(500).json({error: 'Cannot read logs directory'});
+            return res.status(500).json({ error: 'Cannot read logs directory' });
         }
         console.log('[DEBUG] files:', files);
 
         const userLogs = files.filter(file => file.includes(email));
         console.log('[DEBUG] userLogs:', userLogs);
 
-        return res.json({logs: userLogs});
+        return res.json({ logs: userLogs });
     });
 });
 
 router.get('/file', (req, res) => {
     const filename = req.query.filename;
     if (!filename) {
-        return res.status(400).json({error: 'filename is required'});
+        return res.status(400).json({ error: 'filename is required' });
     }
 
-    const logsDir = '/home/ivan/TradingBot/TradingEngine/Logs'
+    const logsDir = '/home/ivan/TradingBot/TradingEngine/Logs';
     const filePath = path.join(logsDir, filename);
 
     if (!fs.existsSync(filePath)) {
-        return res.status(404).json({error: 'File not found'});
+        return res.status(404).json({ error: 'File not found' });
     }
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({error: 'Cannot read log file'});
+            return res.status(500).json({ error: 'Cannot read log file' });
         }
 
-        return res.json({content: data});
+        return res.json({ content: data });
     });
 });
 
